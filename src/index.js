2beb8c8002ee01c530efdb2d82ff123ddcbdfccb
yoyo <jihu777@gmail.com>
2021-02-25 14:02:10 +0800

    fix css

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import "./contextmenu";
 
 const css = document.createElement("link");
 css.rel = "stylesheet";
-css.herf = "https://roam-enhance.vercel.app/main.css";
+css.type = "text/css";
+css.href = "https://roam-enhance.vercel.app/main.css";
 document.getElementsByTagName("head")[0].appendChild(css);
