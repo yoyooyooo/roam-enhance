a72baee81a32cb61e6441e3c0e3e84e59a422c24
yoyo <jihu777@gmail.com>
2021-03-14 22:29:57 +0800

    add plugin:change-fontsize、reading-mode

---
+++ src/plugins/change-fontsize/index.ts
@@ -0,0 +1,33 @@
+import { retry } from "../../utils/common";
+import "./index.css";
+
+retry(() => {
+  ((id) => {
+    let fontSize =
+      +getComputedStyle(document.documentElement).getPropertyValue("--font-size").match(/\d+/)[0] ||
+      14;
+    function changeFontSize(isAdd: boolean, widthStep = 20) {
+      document.documentElement.style.setProperty(
+        "--font-size",
+        `${isAdd ? ++fontSize : --fontSize}px`
+      );
+    }
+
+    let button = document.getElementById(id);
+    button && button.remove();
+
+    const template = document.createElement("div");
+    template.id = id;
+    template.innerHTML = `<span class="chang-font-button bp3-button bp3-minimal bp3-icon-font pointer bp3-small">-</span><span id="${id}" class="chang-font-button bp3-button bp3-minimal bp3-icon-font pointer bp3-small">+</span>`;
+    (template.childNodes[0] as HTMLElement).onclick = () => {
+      changeFontSize(false);
+    };
+    (template.childNodes[1] as HTMLElement).onclick = () => {
+      changeFontSize(true);
+    };
+
+    const topbar = document.querySelector(".rm-topbar");
+    const dot = document.querySelector(".rm-topbar .bp3-popover-wrapper");
+    topbar.insertBefore(template, dot);
+  })("change-font");
+}, "change-font");
--- src/plugins/metadata/index.ts
+++ src/plugins/metadata/index.ts
@@ -3,6 +3,5 @@ import "./index.css";
 
 window.roamEnhance.contextMenu.registerMenu.add(async (menu, clickArea, onClickArgs) => {
   const metaDataMenu = await getMetadataMenu();
-  console.log({ metaDataMenu, menu });
   metaDataMenu && menu.unshift(metaDataMenu);
 });
