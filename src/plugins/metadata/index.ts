612e5ea6050dbb6e3b189c9d0f860445c345280a
yoyo <jihu777@gmail.com>
2021-03-20 21:19:46 +0800

    fix metadata menu

--- src/plugins/metadata/index.ts
+++ src/plugins/metadata/index.ts
@@ -4,7 +4,9 @@ import { getMetadataMenu } from "./metadata";
 
 runPlugin("metadata", () => {
   window.roamEnhance.contextMenu.registerMenu.add(async (menu, clickArea, onClickArgs) => {
-    const metaDataMenu = await getMetadataMenu();
-    metaDataMenu && menu.unshift(metaDataMenu);
+    if (clickArea === "pageTitle" || clickArea === "pageTitle_sidebar") {
+      const metaDataMenu = await getMetadataMenu();
+      metaDataMenu && menu.unshift(metaDataMenu);
+    }
   });
 });
